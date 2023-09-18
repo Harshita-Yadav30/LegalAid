21800877ec121a26a3d2ed285aae47c6ff9d6051
Harshita-Yadav30 <142039652+Harshita-Yadav30@users.noreply.github.com>
2023-09-18 19:31:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ app.use("/api/v1",user);
 app.listen(PORT,() => {
     console.log('App is listening at port ${PORT} ');
 })
+
+app.get('/Create_Account/create_account.html', (req,res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const client = req.body.client;
+    const professional = req.body.professional;
+    const mobile = req.body.mobile;
+    const email = req.body.email;
+    const address = req.body.address;
+    const city = req.body.city;
+    const state = req.body.state;
+})
