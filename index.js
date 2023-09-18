22ee5778ada9dc648503b2b3cfda8a53f31144cf
MohitGarg09 <144173591+MohitGarg09@users.noreply.github.com>
2023-09-18 19:09:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+require("./Config/database").connect();
+
+const user = require("./Routes/user");
+app.use("/api/v1",user);
+
+app.listen(PORT,() => {
+    console.log('App is listening at port ${PORT} ');
+})
